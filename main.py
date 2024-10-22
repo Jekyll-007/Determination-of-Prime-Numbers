96e65c3bee2317ace303cbebe909db09cde3cb1c
Jekyll-007 <teenofeejay@gmail.com>
2024-10-22 15:18:18 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+num=int(input("Enter a number: "))
+if num>1:
+    for i in range(2,int(num**0.5)*1):
+        if num % i==0:
+            print(num,"is not a prime")
+            break
+        else:
+            print(num,"is a prime")
+else:
+    print(num,"is not prime") 
